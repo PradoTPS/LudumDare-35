e97f043ef32b3af2d0d91d42f3955dd3f7bed0eb
Joao Ricardo <jrflga@gmail.com>
2016-04-16 19:57:49 -0300

    Old TV fade and... uh.. :koala:

---
+++ Assets/Resources/Scripts/SceneManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	private bool canFade = false;
+
+	void Update () {
+		if (Input.anyKey) canFade = true;
+		if (canFade) {
+			if (Camera.main.GetComponent<ScreenTransitionImageEffect> ().maskValue < 1f)
+				Camera.main.GetComponent<ScreenTransitionImageEffect> ().maskValue += 0.02f;
+			else
+				Application.LoadLevel (1);
+		}
+	}
+}
