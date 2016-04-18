6a0aa6d73747689c6780ef7d36338c31cb9a9d9a
ThiagoZx <TheThiagoo0@gmail.com>
2016-04-18 17:07:25 -0300

    AvellarTest#01
    Just do it

--- Assets/Resources/Scripts/AnimationManager.cs
+++ Assets/Resources/Scripts/AnimationManager.cs
@@ -18,20 +18,31 @@ public class AnimationManager : MonoBehaviour {
 		iddle = (moveX == 0 && moveY == 0);
 
 		if (iddle) {
-			
 			if (direction > 0) {
 				GetComponent<Animator> ().Play ("IddleBack");			
 			} else {
 				GetComponent<Animator> ().Play ("Iddle");
 			}
-
 		} else {
-			
 			if (moveX != 0 && moveY == 0) {
 				if (moveX > 0) {
 					GetComponent<Animator> ().Play ("WalkingRight");
 				} else {
 					GetComponent<Animator> ().Play ("WalkingLeft");
+				}
+			} else if (moveX != 0 && moveY != 0) {
+ 				
+			} else if (moveX == 0 && moveY > 0) {
+				if (direction >= 179) {
+					GetComponent<Animator> ().Play ("WalkingUpLeft");
+				} else {
+					GetComponent<Animator> ().Play ("WalkingUpRight");
+				}
+			} else if (moveX == 0 && moveY < 0) {
+				if (direction >= 179) {
+					GetComponent<Animator> ().Play ("WalkingLeft");			
+				} else {
+					GetComponent<Animator> ().Play ("WalkingRight");
 				}
 			}
 		}
