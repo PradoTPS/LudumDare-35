29ebb3c5bfbefcde8c8f2f160e38b73aacc18fce
ThiagoZx <TheThiagoo0@gmail.com>
2016-04-18 20:14:56 -0300

    AvellarTest#02
    Now with all the sounds :cactus:

--- Assets/Resources/Scripts/AnimationManager.cs
+++ Assets/Resources/Scripts/AnimationManager.cs
@@ -7,42 +7,40 @@ public class AnimationManager : MonoBehaviour {
 
 	public KeyboardController Kcontroller;
 	public GameObject pivot;
-	bool iddle;
+	private bool idle;
+	private float lastPivotDir;
 
 	void Update () {
 	
 		float moveX = KCI.GetAxisRaw (KeyboardAxis.Horizontal, Kcontroller);
 		float moveY = KCI.GetAxisRaw (KeyboardAxis.Vertical, Kcontroller);
-		float direction = pivot.transform.rotation.eulerAngles.z;
+		float pivotDir = pivot.transform.rotation.eulerAngles.z;
+		string dir = (-1 < pivotDir && pivotDir < 1) ? "right" : (179 < pivotDir && pivotDir < 181) ? "left" : "left";
 
-		iddle = (moveX == 0 && moveY == 0);
+		print (dir);
 
-		if (iddle) {
-			if (direction > 0) {
-				GetComponent<Animator> ().Play ("IddleBack");			
+		idle = (moveX == 0 && moveY == 0);
+
+		if (idle) {
+			if (pivotDir > 0) {
+				GetComponent<Animator> ().Play ("IdleBack");			
 			} else {
-				GetComponent<Animator> ().Play ("Iddle");
+				GetComponent<Animator> ().Play ("Idle");
 			}
 		} else {
 			if (moveX != 0 && moveY == 0) {
-				if (moveX > 0) {
-					GetComponent<Animator> ().Play ("WalkingRight");
-				} else {
+				if ((Input.GetKey (KeyCode.A) && Kcontroller.ToString() == "First") || (Input.GetKey (KeyCode.LeftArrow) && Kcontroller.ToString() == "Second")) {
 					GetComponent<Animator> ().Play ("WalkingLeft");
-				}
-			} else if (moveX != 0 && moveY != 0) {
- 				
-			} else if (moveX == 0 && moveY > 0) {
-				if (direction >= 179) {
-					GetComponent<Animator> ().Play ("WalkingUpLeft");
-				} else {
-					GetComponent<Animator> ().Play ("WalkingUpRight");
-				}
-			} else if (moveX == 0 && moveY < 0) {
-				if (direction >= 179) {
-					GetComponent<Animator> ().Play ("WalkingLeft");			
-				} else {
+				} else if ((Input.GetKey (KeyCode.D) && Kcontroller.ToString() == "First") || (Input.GetKey (KeyCode.RightArrow) && Kcontroller.ToString() == "Second")) {
 					GetComponent<Animator> ().Play ("WalkingRight");
+				}
+			} else if (moveX == 0 && moveY != 0) {
+				if (moveY > 0) {
+					if (dir == "right" ) { GetComponent<Animator> ().Play ("WalkingUpRight"); }
+					if (dir == "left") { GetComponent<Animator> ().Play ("WalkingUpLeft"); }
+				} else {
+					if (dir == "right") { GetComponent<Animator> ().Play ("WalkingRight"); }
+					if (dir == "left") { GetComponent<Animator> ().Play ("WalkingLeft"); }
 				}
 			}
 		}
